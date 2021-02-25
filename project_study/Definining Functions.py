123132028c20087cf03c0233aa94d379724e416e
Tunastrom <falbok10@daum.net>
2021-02-25 14:54:28 +0000

    2021.02.25 into_the_python

--- project_study/Definining Functions.py
+++ project_study/Definining Functions.py
@@ -1,10 +1,10 @@
-def fib(n):
+def fib(n): # -> n = formal parameter
         a, b = 0, 1
         while a < n:
             print(a, end=' ')
             a, b = b, a+b
         print()
-fib(2000)
+fib(2000) # -> 2000 actual parameter
 
 # def + <function name> + parenthesized list of formal parameters
 
@@ -12,8 +12,18 @@ fib(2000)
 
 # execution of a function introduces a nwe symbol table used for the local variables of the fucntion
 
+# sequence of referencing variables
+# local symbol table -> local symbol tables of enclosing function -> global symbol table -> table of built-in name
+#         1                                 2                                  3                      4         
+#                       ***** can not assign the value directly to these variables *****       
 # 
+ 
+# The actual parameters to a function call are introduced in the local symbol table of the called function when it is called
+# -> arguments are passed using call by value (wherer the )
 
+
+
+#### parameter: formal parameter / actual parameter
 
 
 
