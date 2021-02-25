d28d65d27e1efbfe67b5e647dc3c1a42f90960f5
Tunastrom <falbok10@daum.net>
2021-02-25 15:11:21 +0000

    2021.02.26 into_the_python

--- project_study/recursive_function.py
+++ project_study/recursive_function.py
@@ -1,6 +1,7 @@
 ### recursive function
 
 def factorial(n):
+
     if (n == 1):
         return 1
     
@@ -8,4 +9,30 @@ def factorial(n):
 
 print(factorial(3))
 
+def recursion_sum(n,m):
 
+    if m >= n:
+        return m + recursion_sum(n,m-1)
+    else:
+        return 0
+
+ 
+## tail recursion - saving resorces, but same looping times and time complexity
+
+def tail_recursion_sum(total, n, m):
+
+    if m >= n:
+        return tail_recursion_sum(total + m, n, m-1)
+    else:
+        return total
+  
+## loop  
+
+def loop_sum(n,m):
+
+    total = 0 
+    while m >= n:
+        total += m
+        m -= 1
+
+    return total
