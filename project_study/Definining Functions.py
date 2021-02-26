2bb9716b86cd92722048b1a205050b46ccc6ce23
Tunastrom <falbok10@daum.net>
2021-02-26 14:29:44 +0000

    2021.02.26 into_the_python

--- project_study/Definining Functions.py
+++ project_study/Definining Functions.py
@@ -19,11 +19,36 @@ fib(2000) # -> 2000 actual parameter
 # 
  
 # The actual parameters to a function call are introduced in the local symbol table of the called function when it is called
-# -> arguments are passed using call by value (wherer the )
-
-
+# -> arguments are passed using call by value (where the value is always an object reference, not the value of the object).
 
 #### parameter: formal parameter / actual parameter
 
+# When a funcion calls another function or calls itself recursively, a new local symbol table is created for that call.
 
+# A function definition associates the function name with the function object in the current symbol table. 
+# The interpreter recognizes the object pointed to by that name as a user-defined function.
+# Other names can also point to that same function object and can also be used to access the function
+
+# In python, even functions without a return statement do return a value.
+# This value is called None. = built-in name
+# If it would be the only value written, it is suppressed by Interpreter 
+# but can see that by using print()
+
+print(fib)
+f = fib
+print(f(100))
+
+print(fib(0))
+
+
+def fib2(n):
+    result = []
+    a, b = 0, 1
+    while a < n:
+        result.append(a)
+        a, b = b, a+b
+    return result
+
+f100 = fib2(100)
+print(f100)
 
---
+++ project_study/object.py
@@ -0,0 +1,16 @@
+# object = abstracted data by python
+
+# every object should have identity, type, value.
+# identity will not changed if it is created once.
+# you can think that is a address of object in memory.
+# is Operator compare with two object's identities.
+
+# related functions
+# id()  return the value exchaned to integer from identity.
+# type() return the object's type(it's also object, unchangable)
+
+#mutable object: the values in this are changable.
+
+#immutable object: the values in this are not changable.
+#(But immutable container stored references to mutable objects \
+# can't change stored mutable objects but values in stored mutable object)
