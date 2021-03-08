363a11480d1f7f54ae07d251eafe7903a87820bb
Tunastrom <falbok10@daum.net>
2021-03-08 11:54:53 +0000

    2021.03.08 jump_to_python
    5장 내장함수 완료 및 4장 연습문제 1

---
+++ jump_to_python/04.program_input&output/practice_04.py
@@ -0,0 +1,12 @@
+def is_odd(number):
+    if(number % 2 == 0):
+        print('{}는 짝수'.format(number))        
+    else: print('{}는 홀수'.format(number))
+        
+
+is_odd(1)
+is_odd(2)
+is_odd(156)
+is_odd(999)
+
+
--- jump_to_python/05.class,module,package,library/built-in_function.py
+++ jump_to_python/05.class,module,package,library/built-in_function.py
@@ -105,6 +105,76 @@ print(isinstance(a,Person))
 b = 3
 print(isinstance(b,Person)) 
 
+print(f"{'len()':-^13}")
+print('입력인수: iterable -> 요소의 전체 개수 반환' )
 
+print(f"{'list()':-^13}")
+print('입력인수: iterable -> 리스트로 반환')
 
+print(f"{'map(f, iterable)':-^13}")
+print('입력인수: 함수, iterable\niterable의 각요소를 함수에 대입한 결과 반환')
 
+def two_times(numberList):
+    result = [ ]
+    for number in numberList:
+        result.append(number*2)
+    return result
+
+result = two_times([1,2,3,4])
+print(result)
+
+def two_times1(x):
+    return x*2
+
+print(list(map(two_times1,[1,2,3,4])))
+print(list(map(lambda a: a*2, [1,2,3,4])))
+
+print(f"{'max(iterable)':-^13}")
+print('iterable을 인수로 받아 그 최댓값 반환')
+
+print(f"{'min(iterable)':-^13}")
+print('iterable을 인수로 받아 그 최솟값 반환')
+
+print(f"{'oct(x)':-^13}")
+print('int를 8진수 문자열로 반환')
+
+print(f"{'open(filename, [mode])':-^13}")
+print('파일이름과 읽기방법 입력받아 파일 객체 반환')
+print('w: 쓰기, r: 읽기, a:추가, b:바이너리')
+
+print(f"{'ord(c)':-^13}")
+print('문자의 아스키 코드 값 반환')
+
+print(ord('a'))
+print(ord('0'))
+
+print(f"{'pow(x, y)':-^13}")
+print('x의 y제곱 반환')
+
+print(pow(2,4))
+print(pow(3,3))
+
+print(f"{'range([start,]end[, distance])':-^13}")
+
+print(f"{'round(number[, ndigits])':-^13}")
+print('숫자 반올림. ndigits로 소수점 자리 지정')
+
+print(f"{'sorted(iterable)':-^13}")
+print('입력값 정렬하여 반환')
+
+print(f"{'str(object)':-^13}")
+
+print(f"{'sum(iterable)':-^13}")
+print('입력받은 list, tuple의 총합 반환')
+
+print(f"{'tuple(iterable)':-^13}")
+print('입력받은 iterable tuple로 반환')
+
+print(f"{'type(object)':-^13}")
+
+print(f"{'zip(*iterable)':-^13}")
+print('입력된 iterable의 요소들을 index별로 묶는 함수')
+print(list(zip([1,2,3], [4,5,6], [7,8,9])))
+print('길이가 가장 짧은 iterable과의 교집합 범위가 대상')
+print(list(zip([1,2,3,0], [4,5,6,0], [7,8,9])))
+print(list(zip([1,2,3], [4,5,6], [7,8,9,0])))
